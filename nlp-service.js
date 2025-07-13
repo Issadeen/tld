e0a9f4cb498d13d78cad9fb67ea2f73a2165d5d7
Issaerium <minipb9922@gmail.com>
2025-07-13 21:09:39 +0300

    refactor: streamline NLP initialization and remove redundant training data

--- nlp-service.js
+++ nlp-service.js
@@ -1,77 +1,42 @@
-import { dockStart } from '@nlpjs/core';
+import { DockStart } from '@nlpjs/dock-core';
 import { Nlp } from '@nlpjs/nlp';
 import { LangEn } from '@nlpjs/lang-en-min';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 let nlp;
 
 async function initializeNlp() {
-  const dock = await dockStart({
+  if (nlp) {
+    return nlp;
+  }
+
+  const dock = new DockStart({
     settings: {
       nlp: {
-        forceNER: true,
         languages: ['en'],
+        forceNER: true,
       },
     },
     use: ['Nlp', 'LangEn'],
   });
 
+  dock.register('Nlp', Nlp);
+  dock.register('LangEn', LangEn);
+
+  await dock.start();
   nlp = dock.get('nlp');
-  
-  // Add training data
-  nlp.addLanguage('en');
-
-  // Greetings (3 examples)
-  nlp.addDocument('en', 'hello', 'greetings.hello');
-  nlp.addDocument('en', 'hi', 'greetings.hello');
-  nlp.addDocument('en', 'hey', 'greetings.hello');
-
-  // Truck Status (4 examples)
-  nlp.addDocument('en', 'what is the status of %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'check status of %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'status for %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'get status for %truck_id%', 'truck.status');
-
-  // Truck Query (3 examples)
-  nlp.addDocument('en', 'how many %consignor% trucks loaded %dateRange%', 'truck.query');
-  nlp.addDocument('en', 'get entries for %truck_id%', 'truck.query');
-  nlp.addDocument('en', 'how many %consignor% trucks have left', 'truck.query');
-
-  // Add more training examples for better accuracy
-  nlp.addDocument('en', 'good morning', 'greetings.hello');
-  nlp.addDocument('en', 'good afternoon', 'greetings.hello');
-  nlp.addDocument('en', 'good evening', 'greetings.hello');
-  nlp.addDocument('en', 'greetings', 'greetings.hello');
-
-  // More truck status variations
-  nlp.addDocument('en', 'where is %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'find %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'locate %truck_id%', 'truck.status');
-  nlp.addDocument('en', 'track %truck_id%', 'truck.status');
-
-  // More truck query variations
-  nlp.addDocument('en', 'count %consignor% trucks', 'truck.query');
-  nlp.addDocument('en', 'show %consignor% trucks', 'truck.query');
-  nlp.addDocument('en', 'list %consignor% vehicles', 'truck.query');
-
-  // Remove all truck repair NLP training - let repair be handled by default parsing
-  
-  // Add entities
-  nlp.addNerRule('en', 'truck_id', 'regex', /[A-Z]{3}\s?\d{3}[A-Z]?/);
-  nlp.addNerRule('en', 'consignor', 'regex', /MOK|GAPCO|HASS/i);
-  nlp.addNerRule('en', 'dateRange', 'regex', /yesterday and today|today|yesterday/i);
-
-  // Train the model
-  await nlp.train();
-  console.log('NLP model trained.');
-
+  const modelPath = path.join(__dirname, 'model.nlp');
+  await nlp.load(modelPath);
   return nlp;
 }
 
 async function processNlp(text) {
-  if (!nlp) {
-    await initializeNlp();
-  }
-  return nlp.process('en', text);
+  const localNlp = await initializeNlp();
+  return localNlp.process('en', text);
 }
 
 export { initializeNlp, processNlp };
