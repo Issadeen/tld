d4ff13d02144972a247bac8cbd43d3037594d49d
Issaerium <minipb9922@gmail.com>
2025-07-13 20:57:14 +0300

    refactor: enhance training data with additional examples for greetings and truck queries

--- nlp-service.js
+++ nlp-service.js
@@ -20,21 +20,38 @@ async function initializeNlp() {
   // Add training data
   nlp.addLanguage('en');
 
-  // Greetings
+  // Greetings (3 examples)
   nlp.addDocument('en', 'hello', 'greetings.hello');
   nlp.addDocument('en', 'hi', 'greetings.hello');
   nlp.addDocument('en', 'hey', 'greetings.hello');
 
-  // Truck Status
+  // Truck Status (4 examples)
   nlp.addDocument('en', 'what is the status of %truck_id%', 'truck.status');
   nlp.addDocument('en', 'check status of %truck_id%', 'truck.status');
   nlp.addDocument('en', 'status for %truck_id%', 'truck.status');
   nlp.addDocument('en', 'get status for %truck_id%', 'truck.status');
 
-  // Truck Query
+  // Truck Query (3 examples)
   nlp.addDocument('en', 'how many %consignor% trucks loaded %dateRange%', 'truck.query');
   nlp.addDocument('en', 'get entries for %truck_id%', 'truck.query');
   nlp.addDocument('en', 'how many %consignor% trucks have left', 'truck.query');
+
+  // Add more training examples for better accuracy
+  nlp.addDocument('en', 'good morning', 'greetings.hello');
+  nlp.addDocument('en', 'good afternoon', 'greetings.hello');
+  nlp.addDocument('en', 'good evening', 'greetings.hello');
+  nlp.addDocument('en', 'greetings', 'greetings.hello');
+
+  // More truck status variations
+  nlp.addDocument('en', 'where is %truck_id%', 'truck.status');
+  nlp.addDocument('en', 'find %truck_id%', 'truck.status');
+  nlp.addDocument('en', 'locate %truck_id%', 'truck.status');
+  nlp.addDocument('en', 'track %truck_id%', 'truck.status');
+
+  // More truck query variations
+  nlp.addDocument('en', 'count %consignor% trucks', 'truck.query');
+  nlp.addDocument('en', 'show %consignor% trucks', 'truck.query');
+  nlp.addDocument('en', 'list %consignor% vehicles', 'truck.query');
 
   // Remove all truck repair NLP training - let repair be handled by default parsing
   
